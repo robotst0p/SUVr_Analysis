bbeadc829f5367ce087b5da1caef8a0d2c8a8db9
Tyler Meyer <meyertyler68@gmail.com>
2023-04-17 14:50:35 -0400

    connected cgan, optimized svm training on cpus
    removed synthetic data preprocessing and added processing to training loop after synth data is generated using trained cgan model, used intel library to optimize svm training on cpus

--- SVM_CGAN.py
+++ SVM_CGAN.py
@@ -3,13 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sn
 import random
+import time
 
  
 
 #normalization
 from sklearn.preprocessing import StandardScaler
 
- 
+#cpu training optimization for svm
+from sklearnex import patch_sklearn
+patch_sklearn()
 
 #model/training importing 
 from sklearn.model_selection import train_test_split
@@ -26,18 +29,40 @@ from sklearn.model_selection import LeaveOneOut
 #metrics importing (accuracy, precision, sensitivity, recall)
 from sklearn import metrics
 
- 
-
 #parameter tuning 
 import optuna
 
- 
+
+#tensorflow for cgan model loading
+from tensorflow.keras.layers import Input
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.models import Model
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import load_model
+
+#from keras.utils.np_utils import to_categorical
+from tensorflow.keras.utils import to_categorical
+
+import tensorflow as tf
+from keras.callbacks import TensorBoard
+
+import numpy as np
+import argparse
+import pandas as pd
+import datetime
+
+from lib import gan_aud as gan
+
+#load trained cgan
+generator = load_model('/Users/tyler/Desktop/SUVr_Analysis/SUVr_Analysis/weights/wgan_CingulateSUVR_10499.h5')
+synthetic_suvr = gan.test_generator(generator)
 
 #load in suvr data as pandas dataframe
 raw_dataframe = pd.read_excel('AUD_SUVr_wb_cingulate.xlsx', index_col = 0)
 synth_raw_frame = pd.read_excel('generated_Cingulate_SUVR.xlsx')
 
- 
+
 
 #map subject labels to numerical values 
 raw_dataframe.loc[raw_dataframe["CLASS"] == "AUD", "CLASS"] = 1
@@ -53,11 +78,10 @@ X_df = processed_data.drop(['CLASS'], axis = 1)
 synth_frame_x = synth_raw_frame.drop(['Class'], axis = 1)
 synth_frame_y = synth_raw_frame['Class']
 
- 
 
 synth_frame_y = synth_frame_y.astype(int)
 
- 
+test_synth_x = pd.DataFrame(data = synthetic_suvr[0], columns = X_df.columns)
 
 #convert to numpy array for training 
 X = X_df.copy()
@@ -80,30 +104,12 @@ scaler1 = StandardScaler()
 X_model = scaler1.fit(X)
 X_normal = pd.DataFrame(X_model.transform(X), columns = X_df.columns)
 
- 
-
-
-#z-score normalization
-scaler2 = StandardScaler()
-
- 
-
-#raw_data normalization of feature vector
-X_model2 = scaler2.fit(synth_frame_x)
-synth_X_normal = pd.DataFrame(X_model2.transform(synth_frame_x), columns = X_df.columns)
-
- 
-
- 
 
 #leaveoneout cross validation and decisiontree model creation 
 loo = LeaveOneOut()
 loo.get_n_splits(X)
 
  
-
- 
-
 #store accuracies to determine which synthetic data points to add to dataset
 gan_acc_list = []
 
@@ -170,75 +176,95 @@ optuna_search.best_params_
 current_highest_score= optuna_search.best_score_
 
  
-
-
 succesful_cand_X=pd.DataFrame()#pd.dataframe
 succesful_cand_Y=pd.Series()
 
- 
-
-for row in list(synth_X_normal.index.values):
-    y_pred_list=[]
-    svc = svm.SVC(kernel = "rbf", C=1.7, random_state = 42)    
-
- 
-
-    for train_index, test_index in loo.split(X_normal):
-        
-        X_train, X_test = X_normal.iloc[train_index], X_normal.iloc[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        
-        #y_test_list.append(y_test)
-        
-        #append synthetic point to X_training set
-        synth_cand_x = synth_X_normal.loc[row]
-        
-        X_train_intermediate = X_train.append(synth_cand_x, ignore_index = True) # xtrain intermediate is a orig+synthetic data
-        
-        #append synthetic point to y_training set
-        synth_train_y = synth_frame_y.loc[row]
-        
-        y_train_intermediate=y_train.copy()
-        
-        y_train_intermediate.at[len(y_train) + 1] = synth_train_y
-        
-        #y_train=y_train.rename({27: row})
-        
-        y_train_intermediate = y_train_intermediate.rename({len(y_train_intermediate):row})
-        #####################################################
-        ############### change after decision 
-        X_train_intermediate = X_train_intermediate.append(succesful_cand_X)
-        y_train_intermediate = y_train_intermediate.append(succesful_cand_Y)
-        
-        svc.fit(X_train_intermediate, y_train_intermediate)
-        
-        y_pred = svc.predict(X_test)
-        print(y_pred)
-        #y_test_list.append(y_test[0])
-        
-        y_pred_list.append(y_pred[0])
-        
+#cgan connector notes:
+    #use optuna before loop to fit best parameters for svm --> keep the same
+    #use while loop with subject number threshold conditional on outside
+    #take all preproccessing for synthetic data sets and place inside the loop
+    #need to generate synth data, put into dataframe, normalize, then loop throw rows 
     
-    y_pred_final = pd.Series(y_pred_list)
-    
-    # print("YPRED: ")
-    # print(y_pred_list)
-    del svc
-    print("ROW evaluation: ")
-    print(row)
-    
-    #score = metrics.f1_score(y, y_pred_final)
-    score = metrics.accuracy_score(y, y_pred_final)
+synth_counter = 0
 
- 
-
+while synth_counter <= 27:
+    synthetic_suvr = gan.test_generator(generator)
     
-    #find highest f1 score to compare new f1 score to
-    if score > current_highest_score:
-        current_highest_score = score
-        #X_train_intermediate
-        succesful_cand_X = succesful_cand_X.append(synth_cand_x)
-        succesful_cand_Y = succesful_cand_Y.append(y_train_intermediate[-1:])
+    synth_frame_x = pd.DataFrame(data = synthetic_suvr[0], columns = X_df.columns)
+    synth_frame_y = pd.Series(synthetic_suvr[1])
+    
+    #z-score normalization
+    scaler2 = StandardScaler()
+
+    #raw_data normalization of feature vector
+    X_model2 = scaler2.fit(synth_frame_x)
+    synth_X_normal = pd.DataFrame(X_model2.transform(synth_frame_x), columns = X_df.columns)
+    
+    for row in list(synth_X_normal.index.values):
+        y_pred_list=[]
+        svc = svm.SVC(kernel = "rbf", C=1.7, random_state = 42)    
+    
+        for train_index, test_index in loo.split(X_normal):
+            
+            X_train, X_test = X_normal.iloc[train_index], X_normal.iloc[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            
+            #y_test_list.append(y_test)
+            
+            #append synthetic point to X_training set
+            synth_cand_x = synth_X_normal.loc[row]
+            
+            X_train_intermediate = X_train.append(synth_cand_x, ignore_index = True) # xtrain intermediate is a orig+synthetic data
+            
+            
+            #append synthetic point to y_training set
+            synth_train_y = synth_frame_y.loc[row]
+            
+            y_train_intermediate=y_train.copy()
+            
+            y_train_intermediate.at[len(y_train) + 1] = synth_train_y
+            
+            #y_train=y_train.rename({27: row})
+            
+            y_train_intermediate = y_train_intermediate.rename({len(y_train_intermediate):row})
+            #####################################################
+            ############### change after decision 
+            X_train_intermediate = X_train_intermediate.append(succesful_cand_X)
+            y_train_intermediate = y_train_intermediate.append(succesful_cand_Y)
+            
+            svc.fit(X_train_intermediate, y_train_intermediate)
+            
+            y_pred = svc.predict(X_test)
+            #y_test_list.append(y_test[0])
+            
+            y_pred_list.append(y_pred[0])
+            
+        
+        y_pred_final = pd.Series(y_pred_list)
+        
+        # print("YPRED: ")
+        # print(y_pred_list)
+        del svc
+        
+        #score = metrics.f1_score(y, y_pred_final)
+        score = metrics.accuracy_score(y, y_pred_final)
+    
+     
+        print(score)
+        
+        #find highest f1 score to compare new f1 score to
+        if score > current_highest_score:
+            current_highest_score = score
+            #X_train_intermediate
+            succesful_cand_X = succesful_cand_X.append(synth_cand_x)
+            succesful_cand_Y = succesful_cand_Y.append(y_train_intermediate[-1:])
+            
+            print("ACCURACY INCREASED, SYNTHETIC CANDIDATE ADDED")
+            print("NEW ACCURACY: " + str(score))
+            print(synth_cand_x)
+            time.sleep(1)
+            
+            synth_counter += 1
 
 #fix list:
     #synthetic data is not being appended to training dataframe correctly **FIXED**
@@ -247,9 +273,5 @@ for row in list(synth_X_normal.index.values):
 
     
 #version notes
-#removed feature voting 
-#removed unnecessary "rfe_features" argument from feature voting helper function
-#fixed f1 score metric generation by adjusting y_test_list
-#need to adjust the rest of the models to fix this function call
-#removed recursive feature elimination
-#removed optuna from training loop and replaced it with normal model generation, fitting and prediction
+#removed synthetic data preprocessing and added processing to training loop after synth data is generated for testing
+#optimized model training for cpus 
